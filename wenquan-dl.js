3b465f4dc36ae7f75217716c66933db38a7ce6f3
Peter Huang <huangcx22@mails.tsinghua.edu.cn>
2025-09-22 23:45:21 +0800

    Add license to code.

--- wenquan-dl.js
+++ wenquan-dl.js
@@ -1,3 +1,5 @@
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <https://unlicense.org/>
 // ==UserScript==
 // @name         文泉学堂保护装置
 // @namespace    http://tampermonkey.net/
